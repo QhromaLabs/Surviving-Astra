86c3b4b98b76048550efd975ac283ee0b415c3cd
QhromaLabs <qhromalabs@gmail.com>
2025-08-10 17:24:07 +0300

    Allow dragging player and hide system bars

--- app/src/main/java/co/qhroma/shooterlite/MainActivity.kt
+++ app/src/main/java/co/qhroma/shooterlite/MainActivity.kt
@@ -3,6 +3,9 @@ package co.qhroma.shooterlite
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 /**
  * Hosts the [GameView]. The game runs full screen in portrait mode.
@@ -13,6 +16,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
         gameView = GameView(this)
         setContentView(gameView)
     }
@@ -20,5 +29,13 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         gameView.pauseFromActivity()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            WindowInsetsControllerCompat(window, window.decorView)
+                .hide(WindowInsetsCompat.Type.systemBars())
+        }
     }
 }
