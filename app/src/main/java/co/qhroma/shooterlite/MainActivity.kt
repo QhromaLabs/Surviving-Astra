b7222d9b06ec5123004eddafbcd76cddb91fe695
QhromaLabs <qhromalabs@gmail.com>
2025-08-10 17:23:00 +0300

    Revert "Add drag movement and immersive fullscreen"

--- app/src/main/java/co/qhroma/shooterlite/MainActivity.kt
+++ app/src/main/java/co/qhroma/shooterlite/MainActivity.kt
@@ -3,9 +3,6 @@ package co.qhroma.shooterlite
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
 
 /**
  * Hosts the [GameView]. The game runs full screen in portrait mode.
@@ -16,23 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-        hideSystemUI()
         gameView = GameView(this)
         setContentView(gameView)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        hideSystemUI()
-    }
-
-    private fun hideSystemUI() {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
-            controller.hide(WindowInsetsCompat.Type.systemBars())
-            controller.systemBarsBehavior =
-                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        }
     }
 
     override fun onPause() {
